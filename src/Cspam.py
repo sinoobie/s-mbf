777e57295c23c96aa85263e29bf5c342c2eb4178
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-04-21 21:50:06 +0700

    Add files via upload

--- src/Cspam.py
+++ src/Cspam.py
@@ -8,21 +8,41 @@ recode? ok, but don't delete the author name
 '''
 try:
 	import requests,mechanize,json,os,sys,time
+	from requests import Session as ses
+	from http.cookiejar import LWPCookieJar as kuki
 
-	def login(usr,pws):
-		global br,res
-		br=mechanize.Browser()
-		br.set_handle_robots( False )
-		br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
-		br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
-		br.open('https://mbasic.facebook.com/login')
-		br.select_form(nr=0)
-		br.form["email"] = usr
-		br.form["pass"] = pws
-		res=br.submit().read()
+
+	br=mechanize.Browser()
+	br.set_handle_robots( False )
+	br.set_handle_refresh( mechanize._http.HTTPRefreshProcessor(), max_time = 1 )
+	br.addheaders = [ ( 'User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' ) ]
+
+	def login():
+		print("\n[!] checking cookies")
+		time.sleep(1)
+		s = ses()
+		s.cookies = kuki('toket/kue.txt')
+		try:
+			fil=open('toket/kue.txt')
+			fil.close()
+		except FileNotFoundError:
+			print("[!] cookies not found\n\n[!] please login in your facebook once again")
+			email=input('[?] email/username: ')
+			pw=input('[?] password: ')
+			data = {'email':email,'pass':pw}
+			url='https://mbasic.facebook.com/login'
+
+			res = s.post(url,data=data).text
+			if 'm_sess' in str(res) or 'save-device' in str(res):
+				s.cookies.save()
+			else:
+				exit('[!] fail login into your account')
 
 	def send(id,msg,lop):
-		if 'save-device' in str(res) or 'm_sess' in str(res):
+		try:
+			cjs = kuki('toket/kue.txt')
+			cjs.load()
+			br.set_cookiejar(cjs)
 			for i in range(int(lop)):
 				print("[+] sending messages to "+id)
 				br.open('https://mbasic.facebook.com/messages/thread/'+id+'/')
@@ -33,7 +53,8 @@ try:
 						control.disabled = True
 				br["body"]=msg
 				br.submit()
-		else: exit("[!] login failed")
+		except IndexError:
+			print("[!] fail send messages to "+id)
 
 	def getid(msg,limit):
 		ket=open('toket/token.txt','r').read()
@@ -41,7 +62,6 @@ try:
 		js=json.loads(re)
 		for i in js['friends']['data']:
 			send(i['id'],msg,1)
-#			print(i['id'])
 
 	os.system('clear')
 	print("""
@@ -53,22 +73,17 @@ try:
 """)
 	pil = int(input("/kang-newbie_> "))
 	if pil == 1:
-		usr=input("\n[?] username: ")
-		pws=input("[?] password: ")
+		login()
 		print("\n[Info] type '<n>' for newlines")
 		msg=input("[?] message : ").replace('<n>','\n')
 		id=input("[?] target id: ")
 		lop=input("[?] looping : ")
-		print()
-		login(usr,pws)
 		send(id,msg,lop)
 	elif pil == 2:
-		usr=input("\n[?] username: ")
-		pws=input("[?] password: ")
+		login()
 		print("\n[Info] type '<n>' for newlines")
 		msg=input("[?] message : ").replace('<n>','\n')
 		limit=input("[?] how many: ")
-		login(usr,pws)
 		getid(msg,limit)
 
 except KeyboardInterrupt:
