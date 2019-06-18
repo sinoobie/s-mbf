cd7fce376e5e15df19e02690f0072bea0c19044d
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-06-18 15:48:48 +0700

    Add files via upload

--- src/Backdir.py
+++ src/Backdir.py
@@ -4,14 +4,14 @@ try:
 	os.mkdir('../s-mbf_backupdir')
 except: pass
 try:
-	os.system('mv result ../s-mbf_backupdir')
 	print('[moving] result folder')
+	os.system('mv result ../s-mbf_backupdir')
 except: pass
 try:
-	os.system('mv checker ../s-mbf_backupdir')
 	print('[moving] checker folder')
+	os.system('mv checker ../s-mbf_backupdir')
 except: pass
 try:
-	os.system('mv toket ../s-mbf_backupdir')
 	print('[moving] toket folder')
+	os.system('mv toket ../s-mbf_backupdir')
 except: pass
